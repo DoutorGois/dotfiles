8740887c576b6d4065d906238fc9f4c63b8d0256
Zé Henrique <jhenriquetdg@gmail.com>
2018-02-13 13:22:16 -0300

    Mutable added

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -9,5 +9,6 @@ fi
 
 echo "Updating configuration"
 #(cd ~/dotfiles && time_out 3 git pull && time_out 3 git submodule update --init --recursive)
-(cd ~/dotfiles && git pull && git submodule update --init --recursive)
-source ~/dotfiles/zsh/zshrc.sh
+(cd $DOTPATH && git pull && git submodule update --init --recursive)
+echo $DOTPATH/zsh/zshrc.sh
+source $DOTPATH/zsh/zshrc.sh
